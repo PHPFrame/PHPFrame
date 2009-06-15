595893fcbf8b4caacf5a4320c753a18e293262d1
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-06-15 23:26:33 +0000

    Finished refactoring PHPFrame_Utils_Filter class.

--- src/PHPFrame/Utils/Filter.php
+++ src/PHPFrame/Utils/Filter.php
@@ -33,34 +33,260 @@
 class PHPFrame_Utils_Filter
 {
     /**
+     * An array with the different types of filters
+     * 
+     * @staticvar
+     * @var array
+     */
+    private static $_types=array("default", 
+                                 "int", 
+                                 "boolean", 
+                                 "float", 
+                                 "regexp", 
+                                 "url", 
+                                 "email", 
+                                 "ip");
+    
+    /**
      * Validate data stored in variable
      * 
      * <code>
      *  $validEmail = PHPFrame_Utils_Filter::validate($email, 'email');
-     *  //validEmail will contain either the filtered string or boolean FALSE if validation fails
+     *  // validEmail will contain either the filtered string or 
+     *  // boolean FALSE if validation fails
      * </code>
      * 
-     * @param    mixed    $variable to be evaluated    
-     * @param    string    $type valid types are int, boolean, float, regexp, url, email, ip
-     * @return    mixed    Returns the filtered data, or FALSE if the filter fails
+     * @param int|bool|float|string|array $subject    A value os array of values 
+     *                                                to be tested.
+     * @param string                      $type       The filter type to use.
+     *                                                Valid types are int, boolean,
+     *                                                float, regexp, url, email, ip
+     * @param array                       $options    The filter options array.
+     * @param bool                        $exceptions Switch to indicate whether 
+     *                                                we want to throw exceptions
+     *                                                or simply return FALSE on 
+     *                                                failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since 1.0
      */
-    static function validate($variable, $type='default') 
-    {
-        // Check that passed type is recognised
-        $allowed_types = array("default", "int", "boolean", "float", "regexp", "url", "email", "ip");
-        if (!in_array($type, $allowed_types)) {
-            throw new PHPFrame_Exception('PHPFrame Filter error: Data type not recognised by filter.');
+    public static function validate(
+        $subject, 
+        $type='default', 
+        $options=null, 
+        $exceptions=true
+    ) {
+        // Make sure type is string and make lower case
+        $type = (string) strtolower($type);
+        // Check if filter type is supported
+        if (!in_array($type, self::$_types)) {
+            throw new PHPFrame_Exception("Filter type (".$type.") not supported.");
+        }
+        
+        // Handle primitive value subject
+        if (is_int($subject) 
+            || is_bool($subject) 
+            || is_float($subject) 
+            || is_string($subject)) {
+            $subject = array($subject);
+        }
+        
+        // Check that we are working with an array
+        if (!is_array($subject)) {
+            if ($exceptions) {
+                $exception_msg = "Wrong data type passed as 'subject' to";
+                $exception_msg .= "PHPFrame_Utils_Filter::validate().";
+                $exception_msg .= " Expected (int|bool|float|string|array)";
+                $exception_msg .= " and got (".gettype($subject).")";
+                throw new PHPFrame_Exception($exception_msg);
+            } 
+            return false;
         }
         
         // Make filter constant using passed type
         if ($type == 'default') {
             $filter = FILTER_DEFAULT;
-        }
-        else {
+        } else {
             $filter = "FILTER_VALIDATE_".strtoupper($type);
             eval("\$filter = $filter;");    
         }
         
-        return filter_var($variable, $filter);
+        // Declare array to hold filtered data
+        $filtered_array = array();
+        
+        // Loop through array and validate
+        foreach ($subject as $str) {
+            $filtered = filter_var($str, $filter, $options);
+            if ($filtered === false) {
+                if ($exceptions) {
+                    $exception_msg = "Filter validation failed.";
+                    $exception_msg .= " Value (".$str.") doesn't";
+                    $exception_msg .= " conform to filter (".$filter.")";
+                    throw new PHPFrame_Exception($exception_msg);
+                }
+                return false;
+            }
+            $filtered_array[] = $filtered;
+        }
+        
+        // Validation passed, now return filtered data
+        if (count($filtered_array) == 1) {
+            // Return primitive value
+            return $filtered_array[0];
+        } else {
+            // Return array of values
+            return $filtered_array;
+        }
+    }
+    
+    /**
+     * Validate using default filter
+     * 
+     * @param mixed $subject    The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateDefault($subject, $exceptions=true)
+    {
+        return self::validate($subject, "default", null, $exceptions);
+    }
+    
+    /**
+     * Validate using int filter
+     * 
+     * @param mixed $int        The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateInt($int, $exceptions=true)
+    {
+        return self::validate($int, "int", null, $exceptions);
+    }
+    
+    /**
+     * Validate using boolean filter
+     * 
+     * @param mixed $bool       The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateBoolean($bool, $exceptions=true)
+    {
+        return self::validate($bool, "bool", null, $exceptions);
+    }
+    
+    /**
+     * Validate using float filter
+     * 
+     * @param mixed $float      The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateFloat($float, $exceptions=true)
+    {
+        return self::validate($float, "float", null, $exceptions);
+    }
+    
+    /**
+     * Validate a string or array of srings using a regular expresion
+     * 
+     * @param string|array $subject    The subject(s) to validate
+     * @param string       $regexp     The regular expression
+     * @param bool         $exceptions Switch to indicate whether we want to throw
+     *                                 exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateRegExp($subject, $regexp, $exceptions=true)
+    {
+        // Prepare array with filter options
+        $options = array("options"=>array("regexp"=>$regexp));
+        
+        return self::validate($subject, "regexp", $options, $exceptions);
+    }
+    
+    /**
+     * Validate using URL filter
+     * 
+     * @param mixed $url        The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateURL($url, $exceptions=true)
+    {
+        return self::validate($url, "url", null, $exceptions);
+    }
+    
+    /**
+     * Validate using email filter
+     * 
+     * @param mixed $email      The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateEmail($email, $exceptions=true)
+    {
+        return self::validate($email, "email", null, $exceptions);
+    }
+    
+    /**
+     * Validate using IP filter
+     * 
+     * @param mixed $ip         The subject to be tested
+     * @param bool  $exceptions Switch to indicate whether we want to throw
+     *                          exceptions or simply return FALSE on failure.
+     * 
+     * @static
+     * @access public
+     * @return mixed  Returns the filtered value(s) on success. Throws exception or 
+     *                returns FALSE on failure depending on $exceptions argument.
+     * @since  1.0
+     */
+    public static function validateIP($ip, $exceptions=true)
+    {
+        return self::validate($ip, "ip", null, $exceptions);
     }
 }

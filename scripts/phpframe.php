d792e86f0ac866908420463c655e8eab29e01141
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-07-14 11:12:14 +0000

    Overall working update...

---
+++ scripts/phpframe.php
@@ -0,0 +1,19 @@
+#!/usr/local/zend/bin/php
+<?php
+/**
+ * scripts/phpframe.php
+ * 
+ * PHP version 5
+ * 
+ * @category   MVC_Framework
+ * @package    PHPFrame
+ * @subpackage PHPFrame_CLI
+ * @author     Luis Montero <luis.montero@e-noise.com>
+ * @copyright  2009 E-noise.com Limited
+ * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
+ */
+
+echo "I am the phpframe command";
+exit(0);

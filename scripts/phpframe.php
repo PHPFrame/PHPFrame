40c640cd2372b67dde9aeb472c98442a1593b181
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-07-14 12:41:15 +0000

    Added dependencies to package.xml

--- scripts/phpframe.php
+++ scripts/phpframe.php
@@ -15,5 +15,19 @@
  * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
  */
 
-echo "I am the phpframe command";
-exit(0);
+/**
+ * Set constant containing absolute path to application
+ */
+$abs_path = PEAR_Config::singleton()->get('install_dir');
+$abs_path .= DIRECTORY_SEPARATOR."PHPFrame";
+
+define('_ABS_PATH', $abs_path);
+
+// Include PHPFrame main file
+require_once "PHPFrame.php";
+
+// Include autoloader
+//require_once _ABS_PATH.DIRECTORY_SEPARATOR."src".DIRECTORY_SEPARATOR."autoload.php";
+
+// Fire up PHPFrame
+PHPFrame::Fire();

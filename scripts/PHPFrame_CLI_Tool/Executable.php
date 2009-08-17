3d816c15f8b204eecbe4292a77153d4668c09599
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-08-17 01:30:36 +0000

    Adding missing concatenation operator...

--- scripts/PHPFrame_CLI_Tool/Executable.php
+++ scripts/PHPFrame_CLI_Tool/Executable.php
@@ -7,7 +7,7 @@ if (!class_exists("PEAR_Config")) {
 
 // Build path to CLI tool
 $path_to_cli_tool = PEAR_Config::singleton()->get("php_dir");
-$path_to_cli_tool = DIRECTORY_SEPARATOR;
+$path_to_cli_tool .= DIRECTORY_SEPARATOR;
 $path_to_cli_tool .= "PHPFrame_CLI_Tool".DIRECTORY_SEPARATOR;
 $path_to_cli_tool .= "public".DIRECTORY_SEPARATOR;
 $path_to_cli_tool .= "index.php";

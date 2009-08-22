3fb6e6d5e6175c8beaf109b8449024520283fad0
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-08-22 05:11:34 +0000

    Adding javadoc comments...

--- src/PHPFrame/Application/ACL.php
+++ src/PHPFrame/Application/ACL.php
@@ -1,4 +1,30 @@
 <?php
+/**
+ * PHPFrame/Application/ACL.php
+ * 
+ * PHP version 5
+ * 
+ * @category   MVC_Framework
+ * @package    PHPFrame
+ * @subpackage Application
+ * @author     Luis Montero <luis.montero@e-noise.com>
+ * @copyright  2009 E-noise.com Limited
+ * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version    SVN: $Id$
+ * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
+ */
+
+/**
+ * Access Level List Class
+ * 
+ * @category   MVC_Framework
+ * @package    PHPFrame
+ * @subpackage Application
+ * @author     Luis Montero <luis.montero@e-noise.com>
+ * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
+ * @since      1.0
+ */
 class PHPFrame_Application_ACL extends PHPFrame_Mapper_DomainObject
 {
     protected $groupid;

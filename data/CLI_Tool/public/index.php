3727021a6e6b7f567ef99d38163d4ab9a6435892
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-08-29 12:28:08 +0000

    Overall working update.

--- data/CLI_Tool/public/index.php
+++ data/CLI_Tool/public/index.php
@@ -10,13 +10,13 @@
  * 
  * This is reponsible for firing up the application.
  * 
- * @category   MVC_Framework
- * @package    PHPFrame_CLI_Tool
- * @author     Luis Montero <luis.montero@e-noise.com>
- * @copyright  2009 E-noise.com Limited
- * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @version    SVN: $Id$
- * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
+ * @category  PHPFrame
+ * @package   PHPFrame_AppTemplate
+ * @author    Luis Montero <luis.montero@e-noise.com>
+ * @copyright 2009 E-noise.com Limited
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
  */
 
 /**
--- data/CLI_Tool/src/lang/en-GB/global.php
+++ data/CLI_Tool/src/lang/en-GB/global.php
@@ -4,13 +4,13 @@
  * 
  * PHP version 5
  * 
- * @category   MVC_Framework
- * @package    PHPFrame_AppTemplate
- * @author     Luis Montero <luis.montero@e-noise.com>
- * @copyright  2009 E-noise.com Limited
- * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
- * @version    SVN: $Id$
- * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame_AppTemplate
+ * @category  PHPFrame
+ * @package   PHPFrame_AppTemplate
+ * @author    Luis Montero <luis.montero@e-noise.com>
+ * @copyright 2009 E-noise.com Limited
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame_AppTemplate
  */
 
 class GlobalLang

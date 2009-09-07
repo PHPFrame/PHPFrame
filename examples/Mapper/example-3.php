273b7d630d802ad6586e7db0710e90816b7c6fc1
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-09-07 14:52:22 +0000

    Updated package wide docbook documentation for Mapper package.

--- examples/Mapper/example-3.php
+++ examples/Mapper/example-3.php
@@ -1,28 +1,67 @@
 <?php
 include_once "PHPFrame.php";
 
-// Instantiate generic mapper for PHPFrame_ACL class 
-// and specify XML storage
-$mapper = new PHPFrame_Mapper(
-    "PHPFrame_ACL", 
-    "acl", 
-    PHPFrame_Mapper::STORAGE_XML, 
-    false, 
-    DS."tmp".DS."domain.objects"
-);
+class Customer extends PHPFrame_DomainObject
+{
+    protected $first_name, $last_name, $email;
+    
+    public function getFirstName()
+    {
+        return $this->first_name;
+    }
+    
+    public function getLastName()
+    {
+        return $this->last_name;
+    }
+    
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    
+    public function setFirstName($value)
+    {
+        $this->first_name = $value;
+    }
+    
+    public function setLastName($value)
+    {
+        $this->last_name = $value;
+    }
+    
+    public function setEmail($value)
+    {
+        $this->email = PHPFrame_Filter::validateEmail($value);
+    }
+}
 
-// Instantiate domain object
-$acl = new PHPFrame_ACL(array(
-    "groupid"=>1, 
-    "controller"=>"dummy", 
-    "action"=>"*", 
-    "value"=>"all"
+class CustomerMapper extends PHPFrame_Mapper
+{
+    public function __construct()
+    {
+        parent::__construct("Customer", "#__customers");
+    }
+}
+
+$customer = new Customer(array(
+    "first_name"=>"Homer", 
+    "last_name"=>"Simpson", 
+    "email"=>"homer@simpson.com"
 ));
 
-// Insert new object
-$mapper->insert($acl);
+// Show customer object before insert
+print_r($customer);
 
-// Find objects and iterate through collection
-foreach ($mapper->find() as $item) {
-    print_r($item);
+// Get specialised mapper
+$customer_mapper = new CustomerMapper();
+
+// Save customer object in storage
+$customer_mapper->insert($customer);
+
+// Unset $customer object and get it back from storage to prove it works
+unset($customer);
+
+foreach ($customer_mapper->find() as $customer) {
+    print_r($customer);
 }
---
+++ examples/Mapper/example-4.php
@@ -0,0 +1,28 @@
+<?php
+include_once "PHPFrame.php";
+
+// Instantiate generic mapper for PHPFrame_ACL class 
+// and specify XML storage
+$mapper = new PHPFrame_Mapper(
+    "PHPFrame_ACL", 
+    "acl", 
+    PHPFrame_Mapper::STORAGE_XML, 
+    false, 
+    DS."tmp".DS."domain.objects"
+);
+
+// Instantiate domain object
+$acl = new PHPFrame_ACL(array(
+    "groupid"=>1, 
+    "controller"=>"dummy", 
+    "action"=>"*", 
+    "value"=>"all"
+));
+
+// Insert new object
+$mapper->insert($acl);
+
+// Find objects and iterate through collection
+foreach ($mapper->find() as $item) {
+    print_r($item);
+}

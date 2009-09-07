f152c4e8760b605a035ecfd479e34827450856d6
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-09-07 03:24:56 +0000

    Base package examples updated.

--- examples/Base/array.php
+++ examples/Base/array.php
@@ -4,7 +4,21 @@ include_once "PHPFrame.php";
 $array_obj = new PHPFrame_Array(array(1,2,3));
 $assoc_array_obj = new PHPFrame_Array(array("first"=>1,"second"=>2,3, "array"=>array(1,2,3)));
 
-var_dump($array_obj->isAssoc(), $assoc_array_obj->isAssoc());
-var_dump($array_obj->depth(), $assoc_array_obj->depth());
-
 print_r($array_obj);
+print_r($assoc_array_obj);
+
+if ($array_obj->isAssoc()) {
+    echo "\$array_obj is an associative array\n";
+} else {
+    echo "\$array_obj is NOT an associative array\n";
+}
+
+if ($assoc_array_obj->isAssoc()) {
+    echo "\$assoc_array_obj is an associative array\n";
+} else {
+    echo "\$assoc_array_obj is NOT an associative array\n";
+}
+
+
+echo "\$array_obj has a depth of ".$array_obj->depth()."\n";
+echo "\$assoc_array_obj has a depth of ".$assoc_array_obj->depth()."\n";
--- examples/Base/string.php
+++ examples/Base/string.php
@@ -1,13 +1,10 @@
 <?php
 include_once "PHPFrame.php";
 
-$str_obj = new PHPFrame_String("i am a string");
+$str_obj = new PHPFrame_String("i am a string & and object...");
 
-var_dump(
-    $str_obj, 
-    $str_obj->len(), 
-    $str_obj->html(), 
-    $str_obj->upper(), 
-    $str_obj->upperFirst(), 
-    $str_obj->upperWords()
-);
+echo $str_obj." (".$str_obj->len()." character(s) long)\n";
+echo $str_obj->html()." (HTML friendly string)\n";
+echo $str_obj->upper()." (Upper case)\n";
+echo $str_obj->upperFirst()." (Upper case first character)\n";
+echo $str_obj->upperWords()." (Upper case first character of every word)\n";

54363c7b5dfc2c46d19465eaf9efe8c97892956a
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-09-10 23:29:21 +0000

    Added some core lang vars.

--- src/PHPFrame/Lang/en-GB.php
+++ src/PHPFrame/Lang/en-GB.php
@@ -15,6 +15,16 @@
 
 class PHPFrame_Lang
 {
+    // environment > request
+    const SESSION_ERROR_NO_CLIENT_DETECTED="ERROR: No client could be found!";
+    
+    // application > sysevents
+    const EVENT_TYPE_ERROR="Error";
+    const EVENT_TYPE_WARNING="Warning";
+    const EVENT_TYPE_NOTICE="Notice";
+    const EVENT_TYPE_INFO="Info";
+    const EVENT_TYPE_SUCCESS="Success";
+    
     // utils > filesystem
     const UPLOAD_ERROR_PHP_UP_MAX_FILESIZE="ERROR: PHP upload maximum file size exceeded!";
     const UPLOAD_ERROR_PHP_MAX_FILESIZE="ERROR: PHP maximum file size exceeded!";
@@ -24,9 +34,6 @@ class PHPFrame_Lang
     const UPLOAD_ERROR_FILETYPE="ERROR: File type not valid!";
     const UPLOAD_ERROR_MOVE="ERROR: Could not move file to destination directory!";
     const UPLOAD_ERROR_ATTACK="ERROR: Possible file attack!";
-    
-    // environment > request
-    const SESSION_ERROR_NO_CLIENT_DETECTED="ERROR: No client could be found!";
     
     // User
     const EMAIL_ALREADY_REGISTERED="Email is already registered";

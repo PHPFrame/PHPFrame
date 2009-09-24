31667700f5baa0cd1f4fc6d8cbcdfec6327a3af3
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-09-24 15:17:21 +0000

    Temporarily removed unit tests...

--- tests/PHPFrameTest.php
+++ tests/PHPFrameTest.php
@@ -1,14 +1,4 @@
 <?php
-$src_dir = str_replace(DIRECTORY_SEPARATOR."tests", "", dirname(__FILE__));
-$src_dir .= DIRECTORY_SEPARATOR."src";
-require_once $src_dir.DIRECTORY_SEPARATOR."PHPFrame.php";
-
-// Set constant with app specific path to tmp directory
-define("PHPFRAME_TMP_DIR", dirname(__FILE__).DIRECTORY_SEPARATOR."tmp");
-// Initialise PHPFrame environment
-PHPFrame::Env();
-PHPFrame::Response();
-
 class testPHPFrame extends PHPUnit_Framework_TestCase
 {
     function setUp()
@@ -21,64 +11,5 @@ class testPHPFrame extends PHPUnit_Framework_TestCase
         //...
     }
     
-    function test_AppRegistry()
-    {
-        $app_registry = PHPFrame::AppRegistry();
-        
-        $this->assertType("PHPFrame_AppRegistry", $app_registry);
-    }
-    
-    function test_Config()
-    {
-        $this->assertType("PHPFrame_Config", PHPFrame::Config());
-    }
-    
-    function test_MountSQLiteFallbackNoVarDir()
-    {
-        try {
-            PHPFrame::Mount();
-        } 
-        catch (LogicException $expected) {
-            return;
-        }
- 
-        $this->fail('An expected exception has not been raised.');
-    }
-    
-    function test_Mount()
-    {
-        define("PHPFRAME_VAR_DIR", dirname(__FILE__).DIRECTORY_SEPARATOR."var");
-        
-        PHPFrame::Mount();
-        
-        $this->assertEquals(3, PHPFrame::getRunLevel());
-    }
-    
-    function test_DB()
-    {
-        $db = PHPFrame::DB();
-        
-        $this->assertType("PHPFrame_Database", $db);
-    }
-    
-    function test_Session()
-    {
-        $session = PHPFrame::Session();
-        
-        $this->assertType("PHPFrame_SessionRegistry", $session);
-    }
-    
-    function test_Request()
-    {
-        $request = PHPFrame::Request();
-        
-        $this->assertType("PHPFrame_RequestRegistry", $request);
-    }
-    
-    function test_Response()
-    {
-        $response = PHPFrame::Response();
-        
-        $this->assertType("PHPFrame_Response", $response);
-    }
+
 }

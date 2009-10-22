f07b62a3bec1c0414fd9f37c229683de1374ab00
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-10-22 21:12:12 +0000

    Added some missing javadoc comments

--- src/PHPFrame/Filter/EnumFilter.php
+++ src/PHPFrame/Filter/EnumFilter.php
@@ -1,4 +1,28 @@
 <?php
+/**
+ * PHPFrame/Filter/EnumFilter.php
+ * 
+ * PHP version 5
+ * 
+ * @category  PHPFrame
+ * @package   Filter
+ * @author    Luis Montero <luis.montero@e-noise.com>
+ * @copyright 2009 E-noise.com Limited
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
+ */
+
+/**
+ * Enum Filter
+ * 
+ * @category PHPFrame
+ * @package  Filter
+ * @author   Luis Montero <luis.montero@e-noise.com>
+ * @license  http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link     http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame
+ * @since    1.0
+ */
 class PHPFrame_EnumFilter extends PHPFrame_Filter
 {
     
--- src/PHPFrame/HTTP/DownloadRequestListener.php
+++ src/PHPFrame/HTTP/DownloadRequestListener.php
@@ -6,7 +6,6 @@
  * 
  * @category   PHPFrame
  * @package    HTTP
- * @subpackage Request
  * @author     Luis Montero <luis.montero@e-noise.com>
  * @copyright  2009 E-noise.com Limited
  * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
@@ -20,7 +19,6 @@
  * 
  * @category   PHPFrame
  * @package    HTTP
- * @subpackage Request
  * @author     Luis Montero <luis.montero@e-noise.com>
  * @license    http://www.opensource.org/licenses/bsd-license.php New BSD License
  * @link       http://code.google.com/p/phpframe/source/browse/#svn/PHPFrame

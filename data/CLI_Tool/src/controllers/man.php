78c127aa5a97f5181d72c2e3f4acca00df91688a
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-12-07 03:46:24 +0000

    Added version information to CLI man page.

--- data/CLI_Tool/src/controllers/man.php
+++ data/CLI_Tool/src/controllers/man.php
@@ -34,7 +34,9 @@ class ManController extends PHPFrame_ActionController
     {
         $app_doc = new PHPFrame_AppDoc($this->app()->getInstallDir());
         
-        $str  = "To use the command line tool you will need to specify at ";
+        $str  = PHPFrame::version()."\n\n";
+        $str .= "Usage instructions\n\n------------------\n\n";
+        $str .= "To use the command line tool you will need to specify at ";
         $str .= "least a controller,\nand normally also an action and a number";
         $str .= " of parameters. For example, to get\na configuration parameter";
         $str .= " we would use the 'get' action in the 'config'\ncontroller. ";
@@ -44,7 +46,7 @@ class ManController extends PHPFrame_ActionController
         $str .= "in the config\nfile.\n\n";
         $str .= (string) $app_doc;
         
-        $this->response()->setTitle("Usage instructions");
+        $this->response()->setTitle($this->config()->get("app_name"));
         $this->response()->setBody($str);
     }
 }

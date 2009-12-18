50f77f902604b84a8e813992639b0f576ef0603e
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2009-12-18 21:53:48 +0000

    Added simple test to check how the informer object serialises.

---
+++ tests/PHPFrame/Debug/InformerTest.php
@@ -0,0 +1,31 @@
+<?php
+// Include framework if not inculded yet
+require_once preg_replace("/tests\/.*/", "src/PHPFrame.php", __FILE__);
+
+class PHPFrame_InformerTest extends PHPUnit_Framework_TestCase
+{
+    private $_informer;
+    
+    public function setUp()
+    {
+        PHPFrame::setTestMode(true);
+        
+        $this->_informer = new PHPFrame_Informer(
+            new PHPFrame_Mailer(), 
+            array("lupomontero@gmail.com")
+        );
+    }
+    
+    public function tearDown()
+    {
+        //...
+    }
+    
+    public function test_serialise()
+    {
+    	$serialised   = serialize($this->_informer);
+    	$unserialised = unserialize($serialised);
+    	
+    	$this->assertEquals($this->_informer, $unserialised);
+    }
+}

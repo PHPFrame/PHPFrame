f5b2fd5f4af7aee5f7ae6da15098181d0cde9f38
luis.montero@e-noise.com <luis.montero@e-noise.com@5a038eb0-f372-11dd-ac65-f74f249cfaac>
2010-01-07 10:54:59 +0000

    Updated Database package example.

--- examples/Database/example-1.php
+++ examples/Database/example-1.php
@@ -1,12 +1,16 @@
 <?php
 require_once "PHPFrame.php";
 
+// Get instance of database object, if SQLite database doesn't exist it will be 
+// created automatically
 $db = PHPFrame_Database::getInstance("sqlite:".dirname(__FILE__).DS."data.sql3");
 
+// If the database already has a table called "t1" we drop it
 if ($db->hasTable("t1")) {
     $db->dropTable("t1");
 }
 
+// Create new table object and add two columns to it
 $table = new PHPFrame_DatabaseTable($db, "t1");
 $table->addColumn(new PHPFrame_DatabaseColumn(array(
     "name"    => "id", 
@@ -23,12 +27,16 @@ $table->addColumn(new PHPFrame_DatabaseColumn(array(
     "default" => null
 )));
 
+// Show the table columns we just added to the table object
 print_r($table->getColumns());
 
+// Create the table in the database
 $db->createTable($table);
 
+// Now we add some data to prove that our new table was created
 $db->query("INSERT INTO t1 (name) VALUES ('This is sample data')");
 $db->query("INSERT INTO t1 (name) VALUES ('More sample data')");
 $db->query("INSERT INTO t1 (name) VALUES ('And a little more')");
 
+// Query data from the new table and print it
 print_r($db->fetchAssocList("SELECT * FROM t1"));

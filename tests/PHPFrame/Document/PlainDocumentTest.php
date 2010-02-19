78b68a173ce40ef2c1351856c0112fd4b0be8169
Lupo Montero <lupo@e-noise.com>
2010-02-19 21:43:46 +0000

    Replacing tabs with spaces (again)...

--- tests/PHPFrame/Document/PlainDocumentTest.php
+++ tests/PHPFrame/Document/PlainDocumentTest.php
@@ -20,9 +20,9 @@ class PHPFrame_PlainDocumentTest extends PHPUnit_Framework_TestCase
     
     public function test_construct()
     {
-    	$doc = new PHPFrame_PlainDocument("text/plain", "ISO-8859-1");
-    	
-    	$this->assertEquals("ISO-8859-1", $doc->charset());
+        $doc = new PHPFrame_PlainDocument("text/plain", "ISO-8859-1");
+        
+        $this->assertEquals("ISO-8859-1", $doc->charset());
     }
     
     public function test_charset()
@@ -69,7 +69,7 @@ class PHPFrame_PlainDocumentTest extends PHPUnit_Framework_TestCase
         $this->assertEquals($body.$body, $this->_document->body());
     }
     
-    public function prependBody()
+    public function test_prependBody()
     {
         $body = "Lorem ipsum";
         $this->_document->body($body);
@@ -102,22 +102,22 @@ class PHPFrame_PlainDocumentTest extends PHPUnit_Framework_TestCase
     
     public function test_getIterator()
     {
-    	$title = "A nice title";
-    	$body  = "Some really nice content...";
-    	
-    	$this->_document->title($title);
-    	$this->_document->body($body);
-    	
-    	$array = iterator_to_array($this->_document);
-    	
-    	$this->assertEquals(4, count($array));
-    	$this->assertArrayHasKey("mime_type", $array);
-    	$this->assertArrayHasKey("charset", $array);
-    	$this->assertArrayHasKey("title", $array);
-    	$this->assertArrayHasKey("body", $array);
-    	$this->assertEquals("text/plain", $array["mime_type"]);
-    	$this->assertEquals("UTF-8", $array["charset"]);
-    	$this->assertEquals($title, $array["title"]);
-    	$this->assertEquals($body, $array["body"]);
+        $title = "A nice title";
+        $body  = "Some really nice content...";
+        
+        $this->_document->title($title);
+        $this->_document->body($body);
+        
+        $array = iterator_to_array($this->_document);
+        
+        $this->assertEquals(4, count($array));
+        $this->assertArrayHasKey("mime_type", $array);
+        $this->assertArrayHasKey("charset", $array);
+        $this->assertArrayHasKey("title", $array);
+        $this->assertArrayHasKey("body", $array);
+        $this->assertEquals("text/plain", $array["mime_type"]);
+        $this->assertEquals("UTF-8", $array["charset"]);
+        $this->assertEquals($title, $array["title"]);
+        $this->assertEquals($body, $array["body"]);
     }
 }

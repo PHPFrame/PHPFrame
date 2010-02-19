5ccab1ad6927fde38d990d3078c88ba3883d2970
Lupo Montero <lupo@e-noise.com>
2010-02-19 01:29:34 +0000

    Added unit tests for IMAP class and also cleaned up the class and added docblock comments.

--- src/PHPFrame/Mail/IMAP.php
+++ src/PHPFrame/Mail/IMAP.php
@@ -14,7 +14,16 @@
  */
 
 /**
- * IMAP Class
+ * Throw exception if IMAP extension is not loaded.
+ */
+if (!function_exists("imap_open")) {
+	$msg = "PHP's IMAP extension not loaded!!";
+	throw new RuntimeException($msg);
+}
+
+/**
+ * The IMAP Class is used to connect to IMAP servers, read email and manage 
+ * mailboxes.
  * 
  * @category PHPFrame
  * @package  Mail
@@ -25,77 +34,230 @@
  */
 class PHPFrame_IMAP
 {
-    private $_stream=null;
-    private $_host=null;
-    private $_port=null;
-    private $_user=null;
-    private $_password=null;
-    private $_mailbox_name=null;
-    private $_error=array();
+	/**
+	 * IMAP stream.
+	 * 
+	 * @var resource
+	 */
+    private $_stream;
+    /**
+     * IMAP host name or IP address.
+     * 
+     * @var string
+     */
+    private $_host;
+    /**
+     * IMAP server port.
+     * 
+     * @var int
+     */
+    private $_port;
+    /**
+     * IMAP username
+     * 
+     * @var string
+     */
+    private $_user;
+    /**
+     * IMAP password.
+     * 
+     * @var string
+     */
+    private $_password;
+    /**
+     * Mail box name.
+     * 
+     * @var string
+     */
+    private $_mbox;
+    /**
+     * Mail box info.
+     * 
+     * @var array
+     */
+    private $_mbox_info;
     
     /**
-     * Constructor
+     * Constructor.
+     * 
+     * @param string $host     Host name or IP address of IMAP host.
+     * @param string $user     IMAP username.
+     * @param string $password Password for IMAP account.
+     * @param string $mailbox  [Optional] Default value is 'INBOX'.
+     * @param int    $port     [Optional] Default value is '143'.
+     * 
+     * @return void
+     * @throws RuntimeException On connection failure.
+     * @since  1.0
+     */
+    public function __construct(
+        $host, 
+        $user, 
+        $password, 
+        $mailbox="INBOX", 
+        $port=143
+    ) {
+        $this->_host     = (string) $host;
+        $this->_user     = (string) $user;
+        $this->_password = (string) $password;
+        $this->_mbox     = (string) $mailbox;
+        $this->_port     = (int) $port;
+        
+        // Open mailbox stream
+        $this->_connect();
+    }
+    
+    /**
+     * Magic method invoked when an instance is serialized.
+     * 
+     * @return array
+     * @since  1.0
+     */
+    public function __sleep()
+    {
+    	$this->close();
+    	
+    	return array_keys(get_object_vars($this));
+    }
+    
+    /**
+     * Magic method invoked when a new instance is unserialized.
      * 
      * @return void
      * @since  1.0
      */
-    public function __construct($host, $port, $user, $password, $mailbox_name="INBOX") 
+    public function __wakeup()
     {
-        $this->_host         = $host;
-        $this->_port         = $port;
-        $this->_user         = $user;
-        $this->_password     = $password;
-        $this->_mailbox_name = $mailbox_name;
-        
-        // Build mailbox name
-        $mbox = '{'.$this->_host.':'.$this->_port.'/novalidate-cert}'.$this->_mailbox_name;
-        
-          // Open mailbox stream
-          $this->_stream = @imap_open($mbox, $this->_user, $this->_password);
-          if ($this->_stream === false) {
-              $this->_error[] = imap_last_error();
-              return false;
-          }
+    	$this->_connect();
     }
     
     /**
-     * This function gets messages in the current mailbox.
+     * Connect to IMAP server.
+     * 
+     * @return void
+     * @throws RuntimeException
+     * @since  1.0
+     */
+    private function _connect()
+    {
+    	// Build mailbox name
+        $mbox  = '{'.$this->_host.':'.$this->_port.'/novalidate-cert}';
+        $mbox .= $this->_mbox;
+        
+        $this->_stream = @imap_open($mbox, $this->_user, $this->_password);
+        
+        if ($this->_stream === false) {
+            throw new RuntimeException(imap_last_error());
+        }
+    }
+    
+    /**
+     * Reconnect to server if connection is lost.
+     * 
+     * @param bool $force [Optional] Default value is FALSE, in this case the 
+     *                    connection will only be re-established is needed. To 
+     *                    force a new connection set to TRUE. 
+     * 
+     * @return void
+     * @throws RuntimeException
+     * @since  1.0
+     */
+    public function reconnect($force=false)
+    {
+        $force = (bool) $force;
+        
+        if ($force) {
+            $this->close();
+        }
+        
+        if (is_null($this->_stream) || !imap_ping($this->_stream)) {
+            $this->_connect();
+        }
+    }
+    
+    /**
+     * Get mailbox info.
+     * 
+     * @return array Returns an array with the folling keys:
+     *               - date (current system time formatted according to RFC2822)
+     *               - driver (protocol used: pop3, imap or nntp)
+     *               - mailbox (the mailbox name)
+     *               - nmsgs (number of messages in the mailbox)
+     *               - recent (number of recent messages in the mailbox)
+     * @since  1.0
+     */
+    public function getMailboxInfo()
+    {
+    	if (is_null($this->_mbox_info)) {
+	    	$mbox_check = imap_check($this->_stream);
+	    	
+	        if ($mbox_check === false) {
+	            throw new RuntimeException(imap_last_error());
+	        }
+	        
+	        $this->_mbox_info["date"]    = $mbox_check->Date;
+	        $this->_mbox_info["driver"]  = $mbox_check->Driver;
+	        $this->_mbox_info["mailbox"] = $mbox_check->Mailbox;
+	        $this->_mbox_info["nmsgs"]   = $mbox_check->Nmsgs;
+	        $this->_mbox_info["recent"]  = $mbox_check->Recent;
+    	}
+    	
+        return $this->_mbox_info;
+    }
+    
+    /**
+     * Get messages in the current mailbox.
      * 
      * @return array An array of message objects.
+     * @since  1.0
      */
     public function getMessages() 
     {
-          if (!$this->_stream) {
-              return false;
-          }
+    	// This will only try to reconnect if needed.
+        $this->reconnect();
         
-          // Get mailbox info
-        $mbox_check = imap_check($this->_stream);
-        if ($mbox_check === false) {
-            return false;
-        }
+        $mbox_info = $this->getMailboxInfo();
         
         // Get overview for messages in mailbox
-        $messages = imap_fetch_overview($this->_stream, "1:".$mbox_check->Nmsgs, 0);
+        $messages = imap_fetch_overview(
+            $this->_stream, 
+            "1:".$mbox_info["nmsgs"], 
+            0
+        );
+        
         foreach ($messages as $overview) {
-            $body_structure = imap_fetchstructure($this->_stream, $overview->msgno);
-            $parts = $this->_createPartArray($body_structure);
+            $body = imap_fetchstructure($this->_stream, $overview->msgno);
+            
+            $parts = $this->_createPartArray($body);
             //var_dump($parts);
             for ($i=0; $i<count($parts); $i++) {
-                if ($parts[$i]['part_object']->subtype == 'PLAIN' || $parts[$i]['part_object']->subtype == 'HTML') {
+                if ($parts[$i]['part_object']->subtype == 'PLAIN' 
+                    || $parts[$i]['part_object']->subtype == 'HTML'
+                ) {
                     $key = strtoupper($parts[$i]['part_object']->subtype);
-                    $overview->body[$key] .= imap_fetchbody($this->_stream, $overview->msgno, $parts[$i]['part_number']);
+                    
+                    // We declare an empty body if not set yet
+                    if (!isset($overview->body[$key])) {
+                    	$overview->body = array($key=>"");
+                    }
+                    
+                    $overview->body[$key] .= imap_fetchbody(
+                        $this->_stream, 
+                        $overview->msgno, 
+                        $parts[$i]['part_number']
+                    );
+                    
+                } else {
+                    $part_object = $parts[$i]['part_object'];
+                    
+                    // Store attachment info in body
+                    $overview->body['ATTACHMENTS'][] = array(
+                        "filename"    => $part_object->dparameters[0]->value,
+                        "disposition" => $part_object->disposition,
+                        "bytes"       => $part_object->bytes,
+                        "subtype"     => $part_object->subtype
+                    );
                 }
-                else {
-                    $attachment_info = array();
-                    $attachment_info['filename'] = $parts[$i]['part_object']->dparameters[0]->value;
-                    $attachment_info['disposition'] = $parts[$i]['part_object']->disposition;
-                    $attachment_info['bytes'] = $parts[$i]['part_object']->bytes;
-                    $attachment_info['subtype'] = $parts[$i]['part_object']->subtype;
-                    $overview->body['ATTACHMENTS'][] = $attachment_info;
-                    unset($attachment_info);
-                }
-                
             }
         }
         
@@ -103,9 +265,12 @@ class PHPFrame_IMAP
     }
     
     /**
-     * This function deletes a message from the current mailbox
+     * This function deletes a message from the current mailbox.
      *
-     * @param int $uid Can contain a list of ids separated by commas
+     * @param int $uid Can contain a list of ids separated by commas.
+     * 
+     * @return void
+     * @since  1.0
      */
     function deleteMessage($uid) 
     {
@@ -114,6 +279,12 @@ class PHPFrame_IMAP
         }
     }
     
+    /**
+     * Expunge mailbox.
+     * 
+     * @return void
+     * @since  1.0
+     */
     function expunge() 
     {
         @imap_expunge($this->_stream);
@@ -122,59 +293,96 @@ class PHPFrame_IMAP
     /**
      * Close the current IMAP connection.
      * 
-     * @return    void
+     * @return void
+     * @since  1.0
      */
     public function close() 
     {
         // Close IMAP stream
-        imap_close($this->_stream);
+        @imap_close($this->_stream);
+        $this->_stream = null;
     }
     
+    /**
+     * Create array with message parts.
+     * 
+     * @param stdClass $structure An object returned from imap_fetchstructure().
+     * @param string   $prefix    [Optional]
+     * 
+     * @return array
+     * @since  1.0
+     */
     private function _createPartArray($structure, $prefix="") 
     {
-        if (sizeof($structure->parts) > 0) {
-            foreach ($structure->parts as $count => $part) {
-                $this->_addPartToArray($part, $prefix.($count+1), $part_array);
+    	if (isset($structure->parts) && count($structure->parts) > 0) {
+            foreach ($structure->parts as $key=>$value) {
+                $this->_addPartToArray($value, $prefix.($key+1), $parts_array);
             }
-        }
-        else {
+        } else {
             // Email does not have a seperate mime attachment for text
-            $part_array[] = array('part_number' => $prefix.'1', 'part_object' => $structure);
+            $parts_array[] = array(
+                "part_number" => $prefix."1", 
+                "part_object" => $structure
+            );
         }
         
-        return $part_array;
+        return $parts_array;
     }
     
-    private function _addPartToArray($obj, $partno, &$part_array) 
+    /**
+     * Add part object to array containing message parts.
+     * 
+     * @param stdClass $obj          The part object.
+     * @param string   $partno       The part number.
+     * @param array    &$parts_array Parts array passed by reference.
+     * 
+     * @return void
+     * @since  1.0
+     */
+    private function _addPartToArray($obj, $partno, &$parts_array) 
     {
-        $part_array[] = array('part_number' => $partno, 'part_object' => $obj);
-        // Check to see if the part is an attached email message, as in the RFC-822 type
+        $parts_array[] = array('part_number'=>$partno, 'part_object'=>$obj);
+        
+        // Check to see if the part is an attached email message, as in RFC-822
         if ($obj->type == 2) {
             // Check to see if the email has parts
-            if (sizeof($obj->parts) > 0) {
-                // Iterate here again to compensate for the broken way that imap_fetchbody() handles attachments
+            if (count($obj->parts) > 0) {
+                // Iterate here again to compensate for the broken way that 
+                // imap_fetchbody() handles attachments
                 foreach ($obj->parts as $count => $part) {
-                    if (sizeof($part->parts) > 0) {
+                    if (count($part->parts) > 0) {
                         foreach ($part->parts as $count2 => $part2) {
-                            $this->_addPartToArray($part2, $partno.".".($count2+1), $part_array);
+                            $this->_addPartToArray(
+                                $part2, 
+                                $partno.".".($count2+1), 
+                                $parts_array
+                            );
                         }
-                    }
-                    // Attached email does not have a seperate mime attachment for text
-                    else {
-                        $part_array[] = array('part_number' => $partno.'.'.($count+1), 'part_object' => $obj);
+                    } else {
+                    	// Attached email does not have a seperate mime 
+                    	// attachment for text
+                        $parts_array[] = array(
+                            'part_number' => $partno.'.'.($count+1), 
+                            'part_object' => $obj
+                        );
                     }
                 }
+            } else {
+            	// Not sure if this is possible
+                $parts_array[] = array(
+                    'part_number' => $prefix.'.1', 
+                    'part_object' => $obj
+                );
             }
-            // Not sure if this is possible
-            else {
-                $part_array[] = array('part_number' => $prefix.'.1', 'part_object' => $obj);
-            }
-        }
-        // If there are more sub-parts, expand them out.
-        else {
-            if (sizeof($obj->parts) > 0) {
+        } else {
+        	// If there are more sub-parts, expand them out.
+            if (isset($obj->parts) && count($obj->parts) > 0) {
                 foreach ($obj->parts as $count => $p) {
-                    $this->_addPartToArray($p, $partno.".".($count+1), $part_array);
+                    $this->_addPartToArray(
+                        $p, 
+                        $partno.".".($count+1), 
+                        $parts_array
+                    );
                 }
             }
         }
---
+++ tests/PHPFrame/Mail/IMAPTest.php
@@ -0,0 +1,89 @@
+<?php
+// Include framework if not inculded yet
+require_once preg_replace("/tests\/.*/", "src/PHPFrame.php", __FILE__);
+
+class PHPFrame_IMAPTest extends PHPUnit_Framework_TestCase
+{
+    private $_imap;
+    private $_host = "mail.phpframe.org";
+    private $_user = "tests@phpframe.org";
+    private $_pass = "uZeu4zeeng8Ii5Oh";
+    
+    public function setUp()
+    {
+        PHPFrame::setTestMode(true);
+        
+        $data_dir = preg_replace("/tests\/.*/", "data", __FILE__);
+        PHPFrame::setDataDir($data_dir);
+        
+        $this->_imap = new PHPFrame_IMAP(
+            $this->_host, 
+            $this->_user, 
+            $this->_pass
+        );
+    }
+    
+    public function tearDown()
+    {
+        //...
+    }
+    
+    public function test_getMailboxInfo()
+    {
+    	$array = $this->_imap->getMailboxInfo();
+    	
+    	$this->assertArrayHasKey("date", $array);
+    	$this->assertArrayHasKey("driver", $array);
+    	$this->assertTrue(in_array($array["driver"], array("pop3", "imap","nntp")));
+    	$this->assertArrayHasKey("mailbox", $array);
+    	$this->assertArrayHasKey("nmsgs", $array);
+    	$this->assertArrayHasKey("recent", $array);
+    }
+    
+    public function test_getMessages()
+    {
+        $array = $this->_imap->getMessages();
+        
+        $this->assertType("array", $array);
+        $this->assertType("stdClass", $array[0]);
+        $this->assertObjectHasAttribute("subject", $array[0]);
+        $this->assertObjectHasAttribute("from", $array[0]);
+        $this->assertObjectHasAttribute("to", $array[0]);
+        $this->assertObjectHasAttribute("date", $array[0]);
+        $this->assertObjectHasAttribute("message_id", $array[0]);
+        $this->assertObjectHasAttribute("size", $array[0]);
+        $this->assertObjectHasAttribute("uid", $array[0]);
+        $this->assertObjectHasAttribute("msgno", $array[0]);
+        $this->assertObjectHasAttribute("recent", $array[0]);
+        $this->assertObjectHasAttribute("flagged", $array[0]);
+        $this->assertObjectHasAttribute("answered", $array[0]);
+        $this->assertObjectHasAttribute("deleted", $array[0]);
+        $this->assertObjectHasAttribute("seen", $array[0]);
+        $this->assertObjectHasAttribute("draft", $array[0]);
+        $this->assertObjectHasAttribute("body", $array[0]);
+        $this->assertType("array", $array[0]->body);
+    }
+    
+//    public function test_constructorException()
+//    {
+//    	$this->setExpectedException("Exception");
+//    	
+//    	$imap = new PHPFrame_IMAP(
+//            $this->_host, 
+//            $this->_user, 
+//            $this->_pass
+//        );
+//    }
+    
+    public function test_serialise()
+    {
+        $serialised   = serialize($this->_imap);
+        $unserialised = unserialize($serialised);
+        
+        // We reconnect after first serialisation to have two fully feldged 
+        // objects with a valid imap resource
+        $this->_imap->reconnect();
+        
+        $this->assertEquals(serialize($this->_imap), serialize($unserialised));
+    }
+}

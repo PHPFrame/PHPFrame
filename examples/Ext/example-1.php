9223428457aa8599049a692cbe4fd2eba370d89a
Lupo Montero <lupo@e-noise.com>
2010-02-20 18:19:32 +0000

    Version number updated to 0.0.11 as 0.0.10 is already on the PEAR channel...

--- examples/Ext/example-1.php
+++ examples/Ext/example-1.php
@@ -1,7 +1,7 @@
 <?php
 require "PHPFrame.php";
 
-$ext = new PHPFrame_FeatureInfo();
+$ext = new PHPFrame_PluginInfo();
 $ext->setName("Users");
 $ext->setChannel("dist.phpframe.org");
 $ext->setSummary("This is the summary");
@@ -16,37 +16,9 @@ $ext->setLicense(array(
     "uri"  => "http://www.opensource.org/licenses/bsd-license.php"
 ));
 $ext->setNotes("This are the notes....");
-$ext->setDependencies(array(
-    "required" => array(),
-    "optional" => array(
-        "feature" => array(
-            array(
-                "name"    => "Login",
-                "channel" => "dist.phpframe.org",
-                "min"     => "0.0.1",
-                "max"     => "0.0.1"
-            ),
-            array(
-                "name"    => "Admin",
-                "channel" => "dist.phpframe.org",
-                "min"     => "0.0.1",
-                "max"     => "0.0.1"
-            ),
-            array(
-                "name"    => "Dashboard",
-                "channel" => "dist.phpframe.org",
-                "min"     => "0.0.1",
-                "max"     => "0.0.1"
-            )
-        )
-    )
-));
-$ext->addContent(array("path"=>"src/controllers/users.php", "role"=>"php"));
-$ext->addContent(array("path"=>"src/controllers/users.php", "role"=>"php"));
-$ext->addContent(array("path"=>"src/models/users.php", "role"=>"php"));
 
 $mapper = new PHPFrame_Mapper(
-    "PHPFrame_FeatureInfo", 
+    "PHPFrame_PluginInfo", 
     "features",
     PHPFrame_Mapper::STORAGE_XML,
     false,

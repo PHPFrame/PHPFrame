7c91de3ca30ebffdf87330ccaa587b602119a76f
Lupo Montero <lupo@e-noise.com>
2010-04-09 20:23:20 +0100

    Updating the HTTP subpackage...

---
+++ tests/PHPFrame/HTTP/FeedReaderTest.php
@@ -0,0 +1,21 @@
+<?php
+// Include framework if not inculded yet
+require_once preg_replace("/tests\/.*/", "src/PHPFrame.php", __FILE__);
+
+class PHPFrame_FeedReaderTest extends PHPUnit_Framework_TestCase
+{
+    public function setUp()
+    {
+        //...
+    }
+
+    public function tearDown()
+    {
+        //...
+    }
+
+    public function test_()
+    {
+
+    }
+}
